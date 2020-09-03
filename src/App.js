8f2a4650e4950e5dac2d3aba7364de992a239423
Paul Okello <paulokello273@gmail.com>
2020-09-03 15:38:04 +0300

    spotify authentication complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if(_token){
+      setToken(_token);
+    }
+    console.log("TOKEN 👉: ", token);
+  },[]);
   return (
     <div className="app">
-      <Login />
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ):(
+          <Login />
+        )
+      }
+     
     </div>
   );
 }
