492a21597b06c91897cb3cad896af2bcce2736fc
Paul Okello <paulokello273@gmail.com>
2020-09-03 14:12:25 +0300

    Login component

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div>
+            <img
+                src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+                alt=""
+            />
+        </div>
+    )
+}
+
+export default Login
