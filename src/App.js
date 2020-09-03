dbb8bd63dd140d64fe6afe72bdef7f35d222e7ca
Paul Okello <paulokello273@gmail.com>
2020-09-03 17:06:37 +0300

    complete Context API setup and usage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import React, {useState, useEffect} from 'react';
+import React, { useEffect} from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
 import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  const[{ user, token }, dispatch] = useDataLayerValue();
+
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
     if(_token){
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
       spotify.setAccessToken(_token);
-      spotify.getMe().then(user => {
-        console.log("👨 ->", user);
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: 'SET_USER',
+          user: user, // user
+        });
+       
       })
     }
-    console.log("TOKEN 👉: ", token);
+    
   },[]);
+  console.log("TOKEN 👽: ", token);
+  console.log("User 👨 ", user);
   return (
     <div className="app">
       {
--- src/DataLayer.js
+++ src/DataLayer.js
@@ -7,7 +7,8 @@ import React, {
 export const DataLayerContext = createContext();
 
 export const DataLayer = ({initialState, reducer, children}) => (
-    <DataLayerContext.Provider value={useReducer(initialState, reducer)} >
+    <DataLayerContext.Provider value={useReducer(reducer, initialState)} >
         {children}
     </DataLayerContext.Provider>
 )
+export const useDataLayerValue = () => useContext(DataLayerContext);
