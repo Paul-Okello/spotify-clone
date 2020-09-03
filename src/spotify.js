82f3cb5d505a5d223af4ad65620aa47a86fb1f7c
Paul Okello <paulokello273@gmail.com>
2020-09-03 15:10:06 +0300

    spotify login API

---
+++ src/spotify.js
@@ -0,0 +1,19 @@
+// endpoint -> where to send user to login
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// redirects to this url after loging in
+const redirectUri = "http://localhost:3000/"
+
+const clientId = "24219f957586478082a4634229e92b22";
+
+// capabilities of the app i.e spotify-clone
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+  )}&response_type=token&show_dialog=true`;
