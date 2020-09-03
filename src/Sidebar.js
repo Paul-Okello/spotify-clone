51347bd89a35ac3ad8065c8bbebf7af816bbcabf
Paul Okello <paulokello273@gmail.com>
2020-09-03 18:23:57 +0300

    More styling ...

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,9 @@
-import React from 'react'
-import './Sidebar.css'
-import SidebarOption from './SidebarOption'
+import React from 'react';
+import './Sidebar.css';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+import SidebarOption from './SidebarOption';
 function Sidebar() {
     return (
         <div className='sidebar'>
@@ -9,9 +12,12 @@ function Sidebar() {
              src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
              alt=""
             />
-            <SidebarOption title="Home"/>
-            <SidebarOption title="Search"/>
-            <SidebarOption title="Your Library"/>
+            <SidebarOption  Icon={HomeIcon}title="Home"/>
+            <SidebarOption Icon={SearchIcon} title="Search"/>
+            <SidebarOption  Icon={LibraryMusicIcon} title="Your Library"/>
+            <br />
+            <strong className="sidebar__title">PLAYLISTS</strong>
+            <hr /> 
         </div>
     )
 }
